44530e8d3568b1050ff6c2172a68aaebd4cbed94
Irfan <ib262@cornell.edu>
2022-10-14 21:29:59 -0400

    Initial commit

---
+++ android/app/src/main/kotlin/com/lambdalobsters/brh_fashion_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lambdalobsters.brh_fashion_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  static const id = 'HomeScreen';
+
+  @override
+  Widget build(BuildContext context) {
+    Size screenSize = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
