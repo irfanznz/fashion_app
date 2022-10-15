abc0cc29aeb484048e010286dd8da53786a1dc00
Daniel Thorne <dsb289@cornell.edu>
2022-10-14 21:51:20 -0400

    DanielBranch

---
+++ lib/screens/daniel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Daniel extends StatefulWidget {
+  const Daniel({super.key});
+
+  @override
+  State<Daniel> createState() => _DanielState();
+}
+
+class _DanielState extends State<Daniel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
